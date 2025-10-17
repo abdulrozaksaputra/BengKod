10bd26467df7d71fa5daaa3b9bc870c96b2e75fa
Abdul Rozak Saputra <111202214663@mhs.dinus.ac.id>
2025-10-17 11:22:57 +0700

    tugas 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,25 +1,7 @@
-<?php
-
-namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // Kode bawaan Laravel bisa dihapus atau dikomentari
-        // User::factory(10)->create();
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        // Panggil UserSeeder sesuai instruksi dari gambar
         $this->call([
             UserSeeder::class,
         ]);
