571444b2e456feba122f8a5ab820a1cfd30545da
Abdul Rozak Saputra <111202214663@mhs.dinus.ac.id>
2025-10-21 14:08:52 +0700

    baru

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,5 +1,12 @@
 <?php
 
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
 class UserSeeder extends Seeder
 {
     /**
@@ -20,6 +27,22 @@ class UserSeeder extends Seeder
                 'password' => Hash::make('dokter'),
                 'role' => 'dokter',
             ],
+            [
+                'nama' => 'Pasien',
+                'email' => 'pasien@gmail.com',
+                'password' => Hash::make('pasien'),
+                'role' => 'pasien',
+            ],
+            [
+                'nama' => 'Dr. Ahmad Rahman, Sp.PD',
+                'alamat' => 'Jl. Dokter No. 3, Jakarta',
+                'no_ktp' => '3201234567890004',
+                'no_hp' => '081234567893',
+                'email' => 'dokter.umum@gmail.com',
+                'password' => Hash::make('dokter'),
+                'role' => 'dokter',
+                'id_poli' => 3,
+            ],
         ];
 
         foreach ($users as $user) {
@@ -27,3 +50,4 @@ class UserSeeder extends Seeder
         }
     }
 }
+
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,37 +1,64 @@
-<h2>Register</h2>
-
+{{-- <h2>Login</h2> --}}
+{{--
 @if ($errors->any())
-    <ul style="color:red">
-        @foreach ($errors->all() as $err)
-            <li>{{ $err }}</li>
-        @endforeach
-    </ul>
-@endif
+<p style="color:red">{{ $errors->first() }}</p>
+@endif --}}
 
-<form method="POST" action="{{ route('register') }}">
+{{-- <form method="POST" action="{{ route('login') }}">
     @csrf
-    <label>Nama Lengkap:</label><br>
-    <input type="text" name="nama" required><br>
-
     <label>Email:</label><br>
     <input type="email" name="email" required><br>
 
-    <label>Alamat:</label><br>
-    <input type="text" name="alamat" required><br>
-
-    <label>No HP:</label><br>
-    <input type="text" name="no_hp" required><br>
-
-    <label>No KTP:</label><br>
-    <input type="text" name="no_ktp" required><br>
-
     <label>Password:</label><br>
-    <input type="password" name="password" required><br>
+    <input type="password" name="password" required><br><br>
 
-    <label>Konfirmasi Password:</label><br>
-    <input type="password" name="password_confirmation" required><br><br>
+    <button type="submit">Login</button>
+</form> --}}
 
-    <button type="submit">Daftar</button>
-</form>
+<x-layouts.guest title="Login">
+    <div class="login-box d-flex flex-column justify-content-center align-items-center w-100 vh-100">
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center text-lg">
+                <b>Poli</b>klinik
+            </div>
+            <div class="card-body">
+                <p class="login-box-msg">Login ke akun anda</p>
+                <form action="{{ route('login') }}" method="POST">
+                    @csrf
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Email" name="email">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" placeholder="Password" name="password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
 
-<p>Sudah punya akun? <a href="{{ route('login') }}">Login</a></p>
+                    @if ($errors->any())
+                        <p class="alert alert-danger">{{ $errors->first() }}</p>
+                    @endif
+
+                    <div class="row">
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary btn-block" name="submit">Login</button>
+                        </div>
+                    </div>
+                </form>
+
+                <div class="row mt-3 justify-content-center">
+                    <div class="col-12 text-center">
+                        <span>Belum punya akun? <a href="{{ route('register') }}">Register</a></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layouts.guest>
